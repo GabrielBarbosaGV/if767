2b3ca74420b2d037c2db6244de893e3f62d1929c
Gabriel Barbosa Gaspar Veloso <gbgv@cin.ufpe.br>
2019-09-14 00:39:44 -0300

    Modifying knuth-morris-pratt to use get-occurrences

--- kmp.lisp
+++ kmp.lisp
@@ -1,17 +1,18 @@
 (require 'util (or (probe-file "util.fasl") (probe-file "util.lisp")))
 (require 'borders (or (probe-file "borders.fasl") (probe-file "borders.lisp")))
 
+(defun kmp-increment (text pattern)
+  (let ((borders (get-borders pattern)))
+    (lambda (i)
+      (let* ((slice (subseq text i (+ i (length pattern))))
+	     (mismatch-position (get-mismatch-position slice pattern)))
+	(1+ (+ i (aref borders (max mismatch-position 0))))))))
+
 (defun knuth-morris-pratt (&optional text pattern)
   (when (null text) (setf text (get-text)))
   (when (null pattern)
     (setf pattern (elt *posix-argv* 2)))
-  (let ((occ nil) (borders (get-borders pattern)) (len (length pattern)))
-    (loop
-       for i from 0 upto (- (length text) len) do
-	 (let ((mismatch-position (get-mismatch-position (subseq text i (+ i len)) pattern)))
-	 (if (= -1 mismatch-position)
-	     (push i occ)
-	     (incf i (aref borders mismatch-position)))))
-    (nreverse occ)))
+  (get-occurrences text pattern
+		   :increment-function (kmp-increment text pattern)))
 
 (print (knuth-morris-pratt))
