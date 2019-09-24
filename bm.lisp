da557464283a346ed42c1e80d62f8fefee4816df
Gabriel Barbosa Gaspar Veloso <gbgv@cin.ufpe.br>
2019-09-24 20:50:18 -0300

    Adding complete increment function
    An individual goodsuffix-increment function was added, and another
    boyer-moore-increment function which returns the maximum number
    returned between badchar-increment and goodsuffix-increment

--- bm.lisp
+++ bm.lisp
@@ -1,5 +1,6 @@
 (require 'util (or (probe-file "util.fasl") (probe-file "util.lisp")))
 (require 'badchar (or (probe-file "badchar.fasl") (probe-file "badchar.lisp")))
+(require 'goodsuffix (or (probe-file "goodsuffix.fasl") (probe-file "goodsuffix.lisp")))
 
 (defun badchar-increment (text pattern)
   (let ((badacc (get-badchar-hashfunc pattern)))
@@ -8,8 +9,21 @@
 	     (funcall badacc (aref text (max 1 (+ mp i))))))
 	(max 1 (- mp char-pos))))))
 
+(defun goodsuffix-increment (text pattern)
+  (let ((gs get-goodsuffix-table))
+    (lambda (mp i)
+      (aref gs mp))))
+  
+(defun boyer-moore-increment (text pattern)
+  (let ((bi (badchar-increment text pattern))
+	(gi (goodsuffix-increment text pattern)))
+    (lambda (&rest r)
+      (max
+       (apply bi r)
+       (apply gi r)))))
+
 (defun boyer-moore (text pattern)
   (get-occurrences
    text pattern
    :backwards t
-   :increment-function (badchar-increment text pattern)))
+   :increment-function (boyer-moore-increment text pattern)))
